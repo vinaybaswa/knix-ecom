2904cf3989692c179d51a06fc96ee88e0a5909f7
vinay baswa <vinaybaswa@gmail.com>
2024-12-11 20:14:20 -0500

    final touches

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -14,27 +14,82 @@ export const Navigation = () => {
 
   return (
     <Fragment>
-      <header className="h-16 w-full flex justify-between mb-6 bg-gray-400 sticky z-10 -top-1 opacity-100">
-        <Link to="/" className="h-full w-20 p-2 pt-4">
-          <KnixLogo />
-        </Link>
-        <div className="w-[50%] h-full flex items-center justify-end text-lg">
-          <Link to="shop" className="py-2 px-4">
-            shop all
-          </Link>
-          {categories.map((category) => (
-            <Link key={category} to={`shop/${category}`} className="py-2 px-4">
-              {category}
-            </Link>
-          ))}
-          <CartIcon />
+      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <KnixLogo className="h-8" alt="Knix Logo" />
+          </a>
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="/"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/shop"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Shop All
+                </a>
+              </li>
+              {categories.map((category) => (
+                <li key={category}>
+                  <a
+                    href={`/shop/${category}`}
+                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    {category}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <CartIcon />
+          </div>
+          {isCartOpen && <CartDropdown />}
         </div>
-        {isCartOpen && <CartDropdown />}
-      </header>
+      </nav>
+
       <Outlet />
     </Fragment>
   );
 };
 
 export default Navigation;
-
